d83df7b1e7f529a776358907bc9808a2c0226957
andrea <andrea@MBP-di-Andrea.fritz.box>
2017-05-25 22:12:45 +0200

    Add grid system

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,34 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
     return (
       <div>
-        <h3>Examples</h3>
-        <p>Welcome to examples page!</p>
+        <h1 className="text-center">Examples</h1>
+        <p>Here are a few example locations to try out:</p>
+        <ol>
+          <li>
+            <Link to='/?location=Bemopan'>Bemopan, Belize</Link>
+          </li>
+          <li>
+            <Link to={`/?location=${encodeURIComponent('San José')}`}>San José, Costa Rica</Link>
+          </li>
+          <li>
+            <Link to={`/?location=${encodeURIComponent('San Salvador')}`}>San Salvador, El Salvador</Link>
+          </li>
+          <li>
+            <Link to={`/?location=${encodeURIComponent('Guatemala City')}`}>Guatemala City, Guatemala</Link>
+          </li>
+          <li>
+            <Link to='/?location=Tegucigalpa'>Tegucigalpa, Honduras</Link>
+          </li>
+          <li>
+            <Link to='/?location=Managua'>Managua, Nicaragua</Link>
+          </li>
+          <li>
+            <Link to={`/?location=${encodeURIComponent('Panama City')}`}>Panama City, Panama</Link>
+          </li>
+        </ol>
       </div>
     );
 };
