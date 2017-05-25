97496fa3d2f3e6e5d2cde8d91bbcaa58e70355df
andrea <andrea@MBP-di-Andrea.fritz.box>
2017-05-25 21:18:50 +0200

    Add foundation & jQuery

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -5,9 +5,9 @@ var Nav = (props) => {
     return (
       <div>
         <h2>Nav Component</h2>
-        <IndexLink to="/" activeClassName="active">Get Weather</IndexLink>
-        <Link to="/about" activeClassName="active">About</Link>
-        <Link to="/examples" activeClassName="active">Examples</Link>
+        <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+        <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+        <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
       </div>
     );
 };
